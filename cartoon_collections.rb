f7605a03f92f856f3a9c6968ae644caec2bf4cf3
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-16 23:47:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.all? do |call| {call.length >= 5}
+  planeteer_calls.all? {|call| call.length >= 5}
 end
 
 def find_valid_calls(planeteer_calls)
